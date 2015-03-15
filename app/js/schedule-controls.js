63d1d92c78c100d8bca6a82b8d442bdcb5017f6b
ctopjian <charlietopjian@gmail.com>
2015-03-15 10:00:39 -0500

    schedule controls help with edit and other bugs

--- app/js/schedule-controls.js
+++ app/js/schedule-controls.js
@@ -6,6 +6,44 @@ var pglSchedule =	Tidy.Model.extend({
 	'fc': [
 		{
 			msg: ''
+			,scheduleDeleteData: '{"list":[]}'
+			,updateScheduleDeleteFields: function(){
+				console.log(this.scheduleDeleteData);
+				
+				var newField = '';
+				sdata = $.parseJSON(this.scheduleDeleteData);
+				
+				//console.log(data);
+				$.each(sdata['list'], function(i, item){
+
+
+						//hidden form fields
+						var fId = (typeof(item.id) !== 'undefined')?item.id:0;
+						newField = newField + '<input type="hidden" name="data[CampaignSchedule]['+ i + '][id]" value="'+ fId + '"/>';
+						newField = newField + '<input type="hidden" name="data[CampaignSchedule]['+ i + '][enabled]" value="'+ item.enabled + '"/>';
+						
+
+						//console.log(i);
+						
+				});
+				console.log(newField);
+				$('.schedule-delete-fields').html(newField);
+				
+
+			}
+			,schedInit: function(){
+				this.setScheduleStatusList();
+				this.setScheduleLineData();
+				this.updateScheduleLines();
+			}
+			,scheduleStatusList: '{}'
+			,setScheduleStatusList: function(){
+				var initStatusList	=	$('#schedule-status-list').val();
+				if(initStatusList != ''){
+					this.scheduleStatusList = atob(initStatusList);
+				}
+				console.log('init status list' + this.scheduleStatusList);
+			}
 			,scheduleLineData: '{"list":[]}'
 			,setScheduleLineData: function(){
 				var initLineData = $('#schedule-line-init').val();
@@ -45,23 +83,50 @@ var pglSchedule =	Tidy.Model.extend({
 			}			
 			,createLine: function(sDate, sTime){				
 				var lineList	=	 JSON.parse(this.scheduleLineData);
-				lineList['list'].push({"id": 0,"sdate":sDate,"stime":sTime});
+				lineList['list'].push({"id": 0,"sdate":sDate,"stime":sTime, "enabled": 0, "completed": 0});
 				this.scheduleLineData = JSON.stringify(lineList);
 				
 				this.updateScheduleLines();
 			}
+			,createStatusLabel: function(statusVal){				
+				sList = $.parseJSON(this.scheduleStatusList);
+				var statusLabel	=	'<span class="label label-default">not saved</span>';								
+				$.each(sList, function(i, item){					
+					if((i) == statusVal){												
+						statusLabel = '<span class="label '+ item.className +'">'+ item.name +'</span>'
+					}
+					
+				});
+				return statusLabel;
+			}
+			,createRemoveButton: function(statusVal,i){
+
+				var lineId	= 'new'+i;
+				var removeBtn = '<button class="btn btn-danger btn-xs remove-schedule-line" id="'+lineId+'" data-rec-type="new"><i class="fa fa-trash-o"></i></button>';
+
+				//if its not ZERO, then it is from a database
+				if(statusVal>0){
+					removeBtn = '<button class="btn btn-danger btn-xs remove-schedule-line" id="'+lineId+'" data-rec-type="db"><i class="fa fa-trash-o"></i></button>';
+				}
+
+				return removeBtn;
+			}
 			,updateScheduleLines: function(){
 
-				var defaultStatus	=	'<span class="label label-default">not saved</span>';				
+				var _this = this;
+				var defaultStatus	=	'<span class="label label-default">not saved</span>';								
 				var newLine = '';
 				var newField = '';
 
 				data = $.parseJSON(this.scheduleLineData);
-				console.log(data);
+				//console.log(data);
 				$.each(data['list'], function(i, item){
 
-						var lineId	=	'new'+i;
-						var removeBtn = '<button class="btn btn-danger btn-xs remove-schedule-line" id="'+lineId+'"><i class="fa fa-trash-o"></i></button>';
+						defaultStatus = _this.createStatusLabel(item.completed);
+						var removeBtn = _this.createRemoveButton(item.completed,i);
+
+						//var lineId	=	'new'+i;
+						//var removeBtn = '<button class="btn btn-danger btn-xs remove-schedule-line" id="'+lineId+'" data-rec-type="db"><i class="fa fa-trash-o"></i></button>';
 						
 						newLine = newLine + '<tr class="text-center">';
 						newLine = newLine + '<td>'+item.sdate+'</td>';
@@ -76,19 +141,37 @@ var pglSchedule =	Tidy.Model.extend({
 						newField = newField + '<input type="hidden" name="data[CampaignSchedule]['+ i + '][start_date]" value="'+ item.sdate + '"/>';
 						newField = newField + '<input type="hidden" name="data[CampaignSchedule]['+ i + '][start_time]" value="'+ item.stime + '"/>';						
 
-						console.log(i);
+						//console.log(i);
 						
 				});
 				$('.schedule-line-fields').html(newField);
 				$('#schedule-lines').html(newLine);
 			}
-			,removeScheduleLine: function(lineId){
+			,removeScheduleLine: function(lineId, that){
 
 				//clean the "namespace off"					
 				var nId	=	lineId.replace('new','');
 
 				//parse
 				data = $.parseJSON(this.scheduleLineData);
+
+
+				//*Before we delete it from line array
+				//now check to see if the item was a db item
+				//if so we need to make a list, so it can be deleted
+				//on the dbase side on edit->submit
+				var removeCntl	= that.attr('data-rec-type');
+				
+				var _this = this; //redefine
+
+				if(typeof removeCntl != 'undefined' && removeCntl == 'db'){
+					//call deleteScheduleLine function					
+					console.log('data-rec-type: ' + removeCntl);
+					_this.addDeleteField(data['list'][nId].id);
+				} else {
+					console.log('removeCntl has no data-rec-type');
+				}				
+
 
 				//remove elements based on nId
 				//then remove actual "node"
@@ -99,6 +182,19 @@ var pglSchedule =	Tidy.Model.extend({
 				//reset the local var
 				this.scheduleLineData = JSON.stringify(data);
 				this.updateScheduleLines();
+
+				
+			}
+			,addDeleteField: function(recId){
+
+				
+				var deleteList	=	 JSON.parse(this.scheduleDeleteData);
+				deleteList['list'].push({"id": recId,"enabled": 0});
+				this.scheduleDeleteData = JSON.stringify(deleteList);
+				
+				//now update the fields (write them)
+				this.updateScheduleDeleteFields();
+				
 			}
 
 
@@ -110,7 +206,7 @@ var pglSchedule =	Tidy.Model.extend({
 //Jquery controls handlers
 
 $(document).ready(function(){
-	pglSchedule.fc.setScheduleLineData();
+	pglSchedule.fc.schedInit();
 });
 
 //add schedule line
@@ -120,5 +216,5 @@ $('#add-schedule-line').on('click', function(){
 
 });
 $(document).on('click','.remove-schedule-line', function(){
-	pglSchedule.fc.removeScheduleLine(this.id);
+	pglSchedule.fc.removeScheduleLine(this.id, $(this));
 });
